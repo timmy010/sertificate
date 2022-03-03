6d36935d3585d7c30bfda542aa5fe89649593e2b
tim <timsh010@gmail.com>
2022-03-03 17:42:01 +0500

    Добавлен vscode от Maxgraph И сделана первоначальная адаптивность

--- src/js/components/burger.js
+++ src/js/components/burger.js
@@ -1,33 +1,33 @@
-// const burger = document.querySelector('.burger');
-// const menu = document.querySelector('.header__nav');
-// const overlay = document.querySelector('.overlay');
+const burger = document.querySelector('.burger');
+const menu = document.querySelector('.header__nav');
+const overlay = document.querySelector('.overlay');
 
-// const disScroll = () => {
-//   let pagePosition = window.scrollY;
-//   document.body.classList.add('dis-scroll');
-//   document.body.dataset.position = pagePosition;
-//   document.body.style.top = -pagePosition + 'px';
-// }
+const disScroll = () => {
+  let pagePosition = window.scrollY;
+  document.body.classList.add('dis-scroll');
+  document.body.dataset.position = pagePosition;
+  document.body.style.top = -pagePosition + 'px';
+}
 
-// const enScroll = () => {
-//   let pagePosition = parseInt(document.body.dataset.position, 10);
-//   document.body.style.top = 'auto';
-//   document.body.classList.remove('dis-scroll');
-//   window.scrollTo({
-//     top: pagePosition,
-//     left: 0
-//   });
-//   document.body.removeAttribute('data-position');
-// }
+const enScroll = () => {
+  let pagePosition = parseInt(document.body.dataset.position, 10);
+  document.body.style.top = 'auto';
+  document.body.classList.remove('dis-scroll');
+  window.scrollTo({
+    top: pagePosition,
+    left: 0
+  });
+  document.body.removeAttribute('data-position');
+}
 
-// burger.addEventListener('click', (e) => {
-//   burger.classList.toggle('burger--active');
-//   menu.classList.toggle('header__nav--active');
-//   overlay.classList.toggle('overlay--active');
+burger.addEventListener('click', (e) => {
+  burger.classList.toggle('burger--active');
+  menu.classList.toggle('header__nav--active');
+  overlay.classList.toggle('overlay--active');
 
-//   if (burger.classList.contains('burger--active')) {
-//     disScroll();
-//   } else {
-//     enScroll();
-//   }
-// });
+  if (burger.classList.contains('burger--active')) {
+    disScroll();
+  } else {
+    enScroll();
+  }
+});
