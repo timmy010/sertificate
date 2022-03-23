314898fc530456ff6fcb71b6219392b4fbb19842
tim <timsh010@gmail.com>
2022-03-23 16:22:42 +0500

    Добавлена страница сертификации организаций И изменения в вотчере галпа для html

--- gulpfile.js
+++ gulpfile.js
@@ -264,7 +264,7 @@ const watchFiles = () => {
 
   watch(paths.srcScss, styles);
   watch(paths.srcFullJs, scripts);
-  watch(`${paths.srcPartialsFolder}/*.html`, htmlInclude);
+  watch(`${paths.srcPartialsFolder}/**/*.html`, htmlInclude);
   watch(`${srcFolder}/*.html`, htmlInclude);
   watch(`${paths.resourcesFolder}/**`, resources);
   watch(`${paths.srcImgFolder}/**/**.{jpg,jpeg,png,svg}`, images);
